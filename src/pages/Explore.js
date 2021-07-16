4b971383d77e8d758a1e0f80e8fb7ebfbea8b2d5
slamdunk11 <61729276+slamdunk11@users.noreply.github.com>
2021-07-17 04:01:39 +0900

    🚧Card  적용(반응형)

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -1,7 +1,37 @@
 import React from "react";
+import Card from "../components/Card";
+import styled from "styled-components";
 
 const Explore = () => {
-  return <div>explore</div>;
+  return (
+    <>
+      <CardContainer>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </CardContainer>
+    </>
+    );
 };
+
+const CardContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 20px;
+  
+
+  @media screen and (max-width: 990px) {
+    grid-template-columns: repeat(2, 1fr);
+    
+  }
+
+  `;
 
 export default Explore;
