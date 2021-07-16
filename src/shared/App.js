41b2a2d6a29a100b333ccd320d81edeb6a02dc1f
An Jihyun <hwiyu25@gmail.com>
2021-07-16 21:55:01 +0900

    💄 ThemeProvider 적용

--- src/shared/App.js
+++ src/shared/App.js
@@ -2,15 +2,22 @@ import { BrowserRouter, Route } from "react-router-dom";
 import Explore from "../pages/Explore";
 import Opening from "../pages/Opening";
 import MyPage from "../pages/MyPage";
+import GlobalStyles from "./GlobalStyles";
+import { ThemeProvider } from "styled-components";
+
+import theme from "./theme";
 
 const App = () => {
   return (
     <>
-      <BrowserRouter>
-        <Route path="/" exact component={Explore} />
-        <Route path="/opening/:id" exact component={Opening} />
-        <Route path="/MyPage" exact component={MyPage} />
-      </BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <BrowserRouter>
+          <Route path="/" exact component={Explore} />
+          <Route path="/opening/:id" exact component={Opening} />
+          <Route path="/MyPage" exact component={MyPage} />
+        </BrowserRouter>
+      </ThemeProvider>
     </>
   );
 };
---
+++ src/shared/theme.js
@@ -0,0 +1,21 @@
+const colors = {
+  mainBlue: "rgb(51, 102, 255)",
+  black: "rgb(51, 51, 51)",
+  border: "rgb(236, 236, 236)",
+  lightGray: "rgb(153, 153, 153)",
+  gray: "rgb(102, 102, 102)",
+  bgGray: "rgb(249, 249, 250)",
+};
+
+const device = {
+  mobileLg: `@media screen and (min-width: 550px)`,
+  tablet: `@media screen and (min-width: 768px)`,
+  desktop: `@media screen and (min-width: 1024px)`,
+};
+
+const theme = {
+  colors,
+  device,
+};
+
+export default theme;
