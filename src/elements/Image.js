bf755fe13603aaf3bb69de99a78480559b9d562f
An Jihyun <hwiyu25@gmail.com>
2021-07-18 01:32:36 +0900

    🚧 CompanyCard 생성 및 적용

--- src/elements/Image.js
+++ src/elements/Image.js
@@ -1,5 +1,5 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 const Image = (props) => {
   const { shape, src, size } = props;
@@ -9,23 +9,47 @@ const Image = (props) => {
     size,
   };
 
+  if (shape === "square") {
+    return (
+      <Outer>
+        <ImageSquare {...styles}></ImageSquare>
+      </Outer>
+    );
+  }
+
   return <ImageRectangle {...styles}></ImageRectangle>;
 };
 
 Image.defaultProps = {
   // shape: "Inner",
   src: "https://mean0images.s3.ap-northeast-2.amazonaws.com/4.jpeg",
+  size: "50px",
 };
 
+const SeedStyle = css`
+  background-image: url("${(props) => props.src}");
+  background-size: cover;
+`;
+
+const Outer = styled.div`
+  width: 50px;
+  height: 50px;
+  border: 1px solid #dddddd;
+  overflow: hidden;
+`;
+
 const ImageRectangle = styled.div`
+  ${SeedStyle};
   top: 0;
   left: 0;
   padding-top: 75%;
-  overflow: hidden;
-  border: 1px solid #dddddd;
   border-radius: 5px;
-  background-image: url("${(props) => props.src}");
-  background-size: cover;
+`;
+
+const ImageSquare = styled.div`
+  ${SeedStyle};
+  width: 100%;
+  padding-top: 100%;
 `;
 
 export default Image;
