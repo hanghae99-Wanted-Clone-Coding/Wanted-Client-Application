fb9d0a3fbe7c0bb537f34112a4f27b2560a02a89
slamdunk11 <61729276+slamdunk11@users.noreply.github.com>
2021-07-17 12:10:18 +0900

    🚧 Text 생성 및 적용

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,27 +1,27 @@
 import React from "react";
 import styled from "styled-components";
 import Image from "../elements/Image";
+import Text from "../elements/Text";
 
 const Card = (props) => {
     
     return(
         <>
           <CardBox>
-                <Image />
-              
-              <div>
-                <div>채용공고제목</div>
-                <div>회사</div>
-                <div>한국</div>
-                <div>채용보상금</div>
-              </div>
+              <Image />
+              <Text padding="15px">
+                <Text size="18px" bold margin="0 0 10px 0">{props.title}</Text>
+                <Text size="14px" bold margin="0 0 13px 0">{props.companyName}</Text>
+                <Text size="14px" margin="0 0 13px 0">서울 · 한국</Text>
+                <Text size="13px">채용보상금 1,000,000원</Text>
+              </Text>
           </CardBox>
         </>
         
     );
 }
 
-//반응형 하고 싶었으나 뭔가 이상해짐
+
 const CardBox = styled.div`
     position: relative;
     width: 100%;
@@ -31,5 +31,11 @@ const CardBox = styled.div`
     border: 1px solid black;
     
 `;
+
+Card.defaultProps = {
+  title: "프론트엔드 엔지니어(React)",
+  companyName: "굿닥(goodoc)",
+
+}
 
 export default Card;
---
+++ src/elements/Text.js
@@ -0,0 +1,36 @@
+import React from "react";
+import styled from "styled-components";
+
+const Text = (props) => {
+    const {bold, color, size, children, margin, padding} = props;
+
+    const styles = {
+        bold: bold,
+        color: color,
+        size: size,
+        margin: margin,
+        padding: padding,
+    }
+    return (
+        <TextBox {...styles}>{children}</TextBox>
+    );
+}
+
+Text.defaultProps = {
+    bold: false,
+    color: "#333",
+    size: "14px",
+    margin: false,
+    padding: false,
+};
+
+const TextBox = styled.div`
+    color: ${(props) => props.color};
+    font-size: ${(props) => props.size};
+    font-weight: ${(props) => (props.bold? "600" : "400")};
+    ${(props) => (props.margin? `margin: ${props.margin};` : '')};
+    ${(props) => (props.padding? `padding: ${props.padding};` : '')};
+`;
+
+
+export default Text;
