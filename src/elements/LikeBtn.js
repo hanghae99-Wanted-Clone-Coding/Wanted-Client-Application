979f5498ea948e74de727b36fce78c4d0522735b
slamdunk11 <61729276+slamdunk11@users.noreply.github.com>
2021-07-17 18:08:29 +0900

    💄 LikeBtn 적용

--- src/elements/LikeBtn.js
+++ src/elements/LikeBtn.js
@@ -1,23 +1,32 @@
 import React from "react";
 import styled from "styled-components";
+import { TiHeart } from "react-icons/ti";
+import { flex } from "../mixin";
 
 const LikeBtn = (props) => {
     const {children} = props;
     return(
-        <Btn>{children}</Btn>
+
+        <Btn>
+            <Like />
+            {children}
+        </Btn>
     );
 }
 
 const Btn = styled.button`
+    ${flex};
+    justify-content: center;
     position: absolute;
     right: 5px;
     top: 5px;
     width: 52px;
     height: 25px;
     font-size: 12px;
-    color: white;
-    background-color: ${({theme}) => theme.colors.gray};
-    opacity: 0.7;
+    font-weight: 600;
+    color: #fff;
+    // background-color: ${({theme}) => theme.colors.gray};
+    background: rgba(0,0,0,.3);
     border-radius: 3px;
 
     ${({theme}) => theme.device.tablet} {
@@ -25,7 +34,12 @@ const Btn = styled.button`
         top: 10px;
         width: 60px;
         height: 30px;
-    }
+    }   
+`;
+
+const Like = styled(TiHeart)`
+    font-size: 22px;
+    color: ${({ theme }) => theme.colors.heartGray};
 `;
 
 export default LikeBtn;
