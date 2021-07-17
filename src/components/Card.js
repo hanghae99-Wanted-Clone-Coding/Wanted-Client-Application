75ac0900fbe8f4b3f28c817c23655a7c8096348b
slamdunk11 <61729276+slamdunk11@users.noreply.github.com>
2021-07-17 14:58:31 +0900

    💄 Card font 관련 수정

--- src/components/Card.js
+++ src/components/Card.js
@@ -10,10 +10,10 @@ const Card = (props) => {
           <CardBox>
               <Image />
               <Text padding="15px">
-                <Text size="18px" bold margin="0 0 10px 0">{props.title}</Text>
-                <Text size="14px" bold margin="0 0 13px 0">{props.companyName}</Text>
-                <Text size="14px" margin="0 0 13px 0">서울 · 한국</Text>
-                <Text size="13px">채용보상금 1,000,000원</Text>
+                <Title>{props.title}</Title>
+                <Company>{props.companyName}</Company>
+                <Location>서울 · 한국</Location>
+                <Compensation>채용보상금 1,000,000원</Compensation>
               </Text>
           </CardBox>
         </>
@@ -28,9 +28,44 @@ const CardBox = styled.div`
     max-height: 440px;
     padding-top: clac(300 / 1000 * 100%);
     // calc (이미지 높이 ÷ 이미지 가로 × 100 %)
-    border: 1px solid black;
+    // border: 1px solid black;
     
 `;
+
+const Title = styled.div`
+  font-size: 15px;
+  font-weight: 600;
+  margin-bottom: 10px;
+  ${({theme}) => theme.device.tablet} {
+    font-size: 18px;
+  }
+`;
+
+const Company = styled.div`
+  font-size: 11px;
+  font-weight: 600;
+  margin-bottom: 10px;
+  ${({theme}) => theme.device.tablet} {
+    font-size: 14px;
+  }
+`;
+
+const Location = styled.div`
+  font-size: 11px;
+  ${({theme}) => theme.device.tablet} {
+    font-size: 14px;
+  };
+  color:  ${({theme}) => theme.colors.lightGray};
+  margin-bottom: 10px;
+`;
+
+const Compensation = styled.div`
+  font-size: 13px;
+  color:  ${({theme}) => theme.colors.gray};
+  margin-bottom: 10px;
+`;
+
+  
 
 Card.defaultProps = {
   title: "프론트엔드 엔지니어(React)",
