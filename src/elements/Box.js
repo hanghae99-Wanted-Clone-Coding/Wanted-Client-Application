dfe143701f05e50c45e3f701e3665a0b6468f861
An Jihyun <hwiyu25@gmail.com>
2021-07-21 18:13:09 +0900

    🎨 소셜로그인 -> 일반 로그인, 회원가입으로 변경

--- src/elements/Box.js
+++ src/elements/Box.js
@@ -1,55 +1,42 @@
 import React from "react";
 import styled from "styled-components";
 
-const Box = (props) => {
-    const {bg, color, is_flex, is_grid, width, margin, padding, children, text_align} = props;
-    
-    const styles = {
-        is_flex: is_flex,
-        is_grid: is_grid,
-        width: width,
-        margin: margin,
-        padding: padding,
-        text_align: text_align,
-        bg: bg,
-        color: color,
-    }
-    
-    return(
-        <>
-            <Div {...styles}>{children}</Div>
-        </>
-
-    );
-}
+const Box = ({ _onClick, children, ...rest }) => {
+  return (
+    <>
+      <Div {...rest} onClick={_onClick}>
+        {children}
+      </Div>
+    </>
+  );
+};
 
 Box.defaultProps = {
-    children: null,
-    is_flex: false,
-    is_grid: false,
-    width: "100%",
-    padding: false,
-    margin: false,
-    text_align: "left",
-    bg: false,
-    color: "black",
-}
+  children: null,
+  is_flex: false,
+  is_grid: false,
+  width: "100%",
+  padding: false,
+  margin: false,
+  text_align: "left",
+  bg: false,
+  color: "black",
+  _onClick: () => {},
+};
 
 const Div = styled.div`
-    width: ${(props) => props.width};
-    color: ${(props) => props.color};
-    ${(props) => (props.padding ? `padding: ${props.padding};` : "")}
-    ${(props) => (props.margin ? `margin: ${props.margin};` : "")}
+  width: ${(props) => props.width};
+  color: ${(props) => props.color};
+  ${(props) => (props.padding ? `padding: ${props.padding};` : "")}
+  ${(props) => (props.margin ? `margin: ${props.margin};` : "")}
     ${(props) =>
     props.is_flex
-        ? `display: flex; align-items: center; justify-content: space-between; `
-        : ""}
-    ${(props) =>
-    props.is_grid
-        ? `display: grid;`
-        : ""}
+      ? `display: flex; align-items: center; justify-content: space-between; `
+      : ""}
+    ${(props) => (props.is_grid ? `display: grid;` : "")}
     
-    ${(props) => (props.text_align ? `text-align: ${props.text_align};` : "")}    
+    ${(props) =>
+    props.text_align ? `text-align: ${props.text_align};` : ""}    
     ${(props) => (props.bg ? `background-img: url(${props.bg});` : "")}
 `;
 
--- src/redux/modules/user.js
+++ src/redux/modules/user.js
@@ -6,10 +6,12 @@ import { setCookie, getCookie, deleteCookie } from "../../shared/Cookie";
 // action type
 const LOGIN = "user/LOGIN";
 const LOGOUT = "user/LOGOUT";
+const SIGNUP = "user/SIGNUP";
 
 // action creator
 const login = createAction(LOGIN);
 const logout = createAction(LOGOUT);
+// const signUp = createAction(SIGNUP);
 
 // initialState
 const initialState = {
@@ -19,51 +21,59 @@ const initialState = {
 
 //middleware actions
 const loginDB = (code) => {
-  return function (dispatch, getState, {history}) {
+  return function (dispatch, getState, { history }) {
     axios({
       method: "GET",
       // url: `/api/login?code=${code}`,
-      url: `http://52.79.144.138:8080/api/user/${code}`, 
+      url: `http://52.79.144.138:8080/api/user/${code}`,
       // 보내기로 약속한 url
     })
-    .then((res) => {
-      console.log(res);
+      .then((res) => {
+        console.log(res);
 
-      const ACCESS_TOKEN = res.data.accessToken;
+        const ACCESS_TOKEN = res.data.accessToken;
 
-      setCookie("is_login", `${ACCESS_TOKEN}`);
-      history.replace("/");
-      window.alert("성공");
-    })
-    .catch((err) => {
-      console.log(err);
-      window.alert("로그인에 실패하였습니다")
-      history.replace("/");
-    })
-  }
-}
+        setCookie("is_login", `${ACCESS_TOKEN}`);
+        history.replace("/");
+        window.alert("성공");
+      })
+      .catch((err) => {
+        console.log(err);
+        window.alert("로그인에 실패하였습니다");
+        history.replace("/");
+      });
+  };
+};
 
 const logoutDB = () => {
-  return function (dispatch, getState, {history}) {
+  return function (dispatch, getState, { history }) {
     dispatch(logout());
     window.alert("로그아웃 되었습니다");
     history.replace("/");
-  }
-}
+  };
+};
+
+const signUpDB = (infoObj) => {
+  return function (dispatch, getState, { history }) {
+    window.alert("회원가입 완료");
+  };
+};
 
 // reducer
 export default handleActions(
   {
-    [LOGIN]: (state, action) => produce(state, (draft) => {
-      setCookie("is_login", "success");
-      draft.user = action.payload.user;
-      draft.is_login = true;
-    }),
-    [LOGOUT]: (state, action) => produce(state, (draft) => {
-      deleteCookie("is_login");
-      draft.user = {};
-      draft.is_login = false;
-    }),
+    [LOGIN]: (state, action) =>
+      produce(state, (draft) => {
+        setCookie("is_login", "success");
+        draft.user = action.payload.user;
+        draft.is_login = true;
+      }),
+    [LOGOUT]: (state, action) =>
+      produce(state, (draft) => {
+        deleteCookie("is_login");
+        draft.user = {};
+        draft.is_login = false;
+      }),
   },
   initialState
 );
@@ -72,8 +82,7 @@ const actionCreators = {
   login,
   logout,
   loginDB,
-  logoutDB
-
+  logoutDB,
 };
 
-export {actionCreators};
+export { actionCreators };
