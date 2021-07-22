8f67ad6fea0c724386dd1ac5a40adbcb5962465f
slamdunk11 <61729276+slamdunk11@users.noreply.github.com>
2021-07-22 21:36:25 +0900

    🚧 로그아웃 기능 연결, MiniModal 반응형

--- src/elements/IconBtn.js
+++ src/elements/IconBtn.js
@@ -2,18 +2,21 @@ import React from "react";
 import styled from "styled-components";
 
 const IconBtn = (props) => {
-  const { icon, show } = props;
-  return <Btn show={show ? show : null}>{icon}</Btn>;
+  const { icon, show, _onClick } = props;
+  return <Btn onClick={_onClick} show={show ? show : null}>{icon}</Btn>;
 };
 
 IconBtn.defaultProps = {
   icon: "",
   show: "",
+  _onClick: () => {},
 };
 
 const Btn = styled.button`
   padding: 0 10px;
   font-size: 18px;
 `;
+
+
 
 export default IconBtn;
